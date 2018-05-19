b011d3d3b0137e2f189efe7c9ab261f4999303f4
Suzard <eternalpirateking@hotmail.com>
2018-05-18 20:08:21 -0700

    Update test_construct.py

--- test_construct.py
+++ test_construct.py
@@ -7,5 +7,5 @@ import sys
 import re
 client = MongoClient('localhost', 27017)
 db = client.cs121_db
-
-db.posts.insert_one({"token" : "tokenStr",  "docIds" : "hi"})
+db.posts.remove({"token":"Informatics"})
+db.posts.insert_one({"token" : "Informatics",  "docIds" : ["docid1","docid2","docid3","docid4","docid5", "docid6","docid7","docid8","docid9","docid10", "docid11"]})
