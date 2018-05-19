c4c420179f9842a72cc26048676e419088beae9c
skayani <=>
2018-05-19 15:07:55 -0700

    Added changes to test_search

--- test_search.py
+++ test_search.py
@@ -8,20 +8,35 @@ import re
 
 client = MongoClient('localhost', 27017)
 db = client.cs121_db
+index = db.index
+
+BOOKKEPING_LOC = "../WEBPAGES_RAW/bookkeeping.json"
+f = open("query_results.txt", "w+")
 
 def test_query(query):
     try:
+        query = query.lower()
         count = 0
-        result = db.posts.find({"token": query})
+        result = index.find({"_id": query})
         token_value = result.next()
-        print(token_value)
+        f.write("For query '{}'\n".format(query))
         for ids in token_value["docIds"]:
             if(count == 10):
                 break
-            print(ids)
+            
+            json_data = json.load(open(BOOKKEPING_LOC))
+            f.write(json_data[ids] + '\n')
+            count += 1
+            f.write('\n')
+
+        f.write('\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('\n')
     except StopIteration:
         print("Not in inverted index")
 
 test_query("Informatics")
 test_query("Mondego")
 test_query("Irvine")
+f.close()
