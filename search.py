e60f5b302bc193a8709ab269e3ce8eb2def8bca6
Suzard <eternalpirateking@hotmail.com>
2018-05-28 00:23:31 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import json
 
-BOOKKEPING_LOC = "../Project_3/WEBPAGES_RAW"
+BOOKKEPING_LOC = "WEBPAGES_RAW/bookkeeping.json"
 
 def get_tfidf( key_value_tuple ):
 	return key_value_tuple[1]["tf-idf"]
